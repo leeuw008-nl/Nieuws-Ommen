92174a0e01fdb263592930fdc7efbeaea3a3adcc
leeuw008-nl <leeuw008@gmail.com>
2026-08-08 16:50:09 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,8 @@
-
-const CACHE_NAME = 'nieuws-ommen-v112d-CLEAN';
-self.addEventListener('install', e=>{
-  self.skipWaiting();
-});
+// v112f - 2026-08-08 07:29:23 - 1786199363
+const CACHE_NAME = 'nieuws-ommen-v112f-1786199363';
+self.addEventListener('install', e=>{ self.skipWaiting(); });
 self.addEventListener('activate', e=>{
-  e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())
-  );
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
 });
 self.addEventListener('fetch', e=>{
   const url = e.request.url;
